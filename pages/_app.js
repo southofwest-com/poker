9f3fbea5dd517ac06cc3f26d22a30b1cc7803686
Sean Martin <sean@southofwest.com>
2025-05-06 10:08:17 +0800

    UI changes

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,10 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
+    <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
         <title>Anonymous Voting Tool</title>
         <meta name="description" content="Anonymous voting tool for teams" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
